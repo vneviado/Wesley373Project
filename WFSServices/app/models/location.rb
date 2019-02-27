74cfa959ea63991a11d4c7312eea7b17e490b942
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-02-27 18:58:12 -0500

    Added relationships and validations to model

--- WFSServices/app/models/location.rb
+++ WFSServices/app/models/location.rb
@@ -1,2 +1,13 @@
 class Location < ApplicationRecord
+  #Relationships
+  has_many :services
+
+  #Validations
+  validates_presence_of :name, :phone_number, :street, :city, :zipcode
+  validates :phone_number, with: { /^\(?(\d{3})\)?[ -.]?(\d{3})[-.]?(\d{4})$/ ,
+    message: "Valid phone numbers only" }
+  validates :zipcode, length: { is: 5 }
+
+  #Scopes
+  #Methods
 end
