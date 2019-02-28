ef1d11085ac8a34a88d4f755931fd5e9420287ad
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-02-27 19:13:09 -0500

    added relationships and validations to contact model

--- WFSServices/app/models/contact.rb
+++ WFSServices/app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  #Relationships
+  has_many :services
+
+  #Validations
+  validates_presence_of :name, :work_number, :phone_number, :street_1, :city_1, :zipcode_1
+  validates :work_number, with: {}
+  validates :phone_number, with: {}
+  validates :zipcode_1, :zipcode_2, length: { is: 5 }
+
+  #Scopes
+  #Methods
 end
