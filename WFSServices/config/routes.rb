80af2b0b4de9231ad41d9ba4342560c946573757
Sandy Pan <sqp@andrew.cmu.edu>
2019-02-28 21:40:35 -0500

    Fixed routes

--- WFSServices/config/routes.rb
+++ WFSServices/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :services
   resources :service_types
   resources :locations
   resources :categories
-=======
   resources :contacts
->>>>>>> e3ea77bb8306497b17a7a86c6fbf1b466db0fe28
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
