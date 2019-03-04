e11abdc89699e95bb226e0582926c33f23e46ec6
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-03-04 10:55:29 -0500

    saved a file I forgot to save

--- WFSServices/config/routes.rb
+++ WFSServices/config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 45a1155ecf2e426dc05d9f32e65adabf7b696bbe
   resources :services
   resources :service_types
   resources :locations
   resources :categories
-<<<<<<< HEAD
-=======
-=======
   resources :locations
->>>>>>> location
-=======
->>>>>>> 45a1155ecf2e426dc05d9f32e65adabf7b696bbe
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
