73be4739f34df1387884e102f20bb43a7e9ed767
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-03-06 08:51:59 -0500

    sorry for working on master branch, I just added some quick validations

--- WFSServices/app/models/contact.rb
+++ WFSServices/app/models/contact.rb
@@ -4,9 +4,10 @@ class Contact < ApplicationRecord
 
   #Validations
   validates_presence_of :name, :work_number, :phone_number, :street_1, :city_1, :zipcode_1
-  validates :work_number, with: {}
-  validates :phone_number, with: {}
+  validates :work_number, :phone_number, with: { /^\(?(\d{3})\)?[ -.]?(\d{3})[-.]?(\d{4})$/ ,
+    message: "Valid phone numbers only" }
   validates :zipcode_1, :zipcode_2, length: { is: 5 }
+  validates :zipcode_1, :zipcode_2, with: { /^[0-9]{5}(?:-[0-9]{4})?$/ }
 
   #Scopes
   #Methods
--- WFSServices/app/models/location.rb
+++ WFSServices/app/models/location.rb
@@ -7,6 +7,7 @@ class Location < ApplicationRecord
   validates :phone_number, with: { /^\(?(\d{3})\)?[ -.]?(\d{3})[-.]?(\d{4})$/ ,
     message: "Valid phone numbers only" }
   validates :zipcode, length: { is: 5 }
+  validates :zipcode, with: { /^[0-9]{5}(?:-[0-9]{4})?$/ }
 
   #Scopes
   #Methods
