0481d0c2113d079e59f2efb18f21cff32a3dcf37
Sandy Pan <sqp@andrew.cmu.edu>
2019-03-24 21:50:16 -0400

    Category tests done

--- WFSServices/test/models/category_test.rb
+++ WFSServices/test/models/category_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Relationship Matchers
+  should have_many(:services)
+
+  # Validation Matcher
+  should validate_presence_of(:name)
+  
+  # Context
+  context "Within context" do
+    setup do
+      create_categories
+    end
+
+    teardown do
+      destroy_categories
+    end
+
+    # Scope Testss
+    should "have an alphabetical scope to order by name" do
+      assert_equal [@autism, @justice], Category.alphabetical.to_a
+    end
+
+  end
+
 end
