122fa9c831e24980f7607d1af46096233916e36a
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-03-29 00:53:36 -0400

    Location index and show page

--- WFSServices/test/sets/locations.rb
+++ WFSServices/test/sets/locations.rb
@@ -2,13 +2,12 @@ module Contexts
     module Locations
       def create_locations
         @beta = FactoryBot.create(:location)
-        @wilkinsburg = FactoryBot.create(:location, name:"Wilkinsburg", phone_number:"4121234577", street:"221 Penn Ave")  
+        @wilkinsburg = FactoryBot.create(:location, name:"Wilkinsburg", phone_number:"4121234577", street:"221 Penn Ave", city:"Pittsburgh", zipcode:"15217")  
       end
-      
+
       def destroy_locations
         @beta.delete
         @wilkinsburg.delete
       end
     end
 end
-  
