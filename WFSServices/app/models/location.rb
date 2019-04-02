f1e49e8be3d13ecabd1d822f7c5ec51834df4295
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-04-02 11:02:29 -0400

    Most of the Locations show and index page done

--- WFSServices/app/models/location.rb
+++ WFSServices/app/models/location.rb
@@ -12,8 +12,11 @@ class Location < ApplicationRecord
   validates_format_of :zipcode, with: /\A\d{5}\z/, message: "should be five digits long", allow_blank: true
 
   #Scopes
+
+
   #Methods
   def self.for_service(service)
     service.locations.to_a
   end
+
 end
