7a12f6bc63a835fae40a515a8c7862ffa4dfd880
Sandy Pan <sqp@andrew.cmu.edu>
2019-04-16 10:17:34 -0400

    changed search for services to accommodate searching by description, philosopy, and level of care

--- WFSServices/app/models/service.rb
+++ WFSServices/app/models/service.rb
@@ -15,5 +15,7 @@ class Service < ApplicationRecord
 
     # Scopes
     scope :alphabetical, -> { order( 'name' ) }
-    scope :search, ->(term) { Service.joins(:category).where('services.name LIKE ? OR categories.name LIKE ?', "%#{term}%", "%#{term}%") }
+    scope :search, ->(term) { Service.joins(:category).where('services.name LIKE ? 
+    OR categories.name LIKE ? OR services.description LIKE ? OR services.philosophy LIKE ? 
+    OR services.level_of_care LIKE ?', "%#{term}%", "%#{term}%", "%#{term}%", "%#{term}%", "%#{term}%") }
 end
