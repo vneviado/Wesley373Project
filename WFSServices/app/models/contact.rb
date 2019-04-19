2ef9e09cfd6eef74b30235c4e91b6655c9ad678e
Ryan Hoffman <ryanh1@andrew.cmu.edu>
2019-04-19 11:43:22 -0400

    small fries

--- WFSServices/app/models/contact.rb
+++ WFSServices/app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < ApplicationRecord
   #validates :work_number, :phone_number, with: /^\(?(\d{3})\)?[ -.]?(\d{3})[-.]?(\d{4})$/, message: "Valid phone numbers only"
 
   validates_format_of :work_number, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only"
-  validates_format_of :phone_number, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only"
+  validates_format_of :phone_number, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only", allow_blank: true
 
   #validates :work_number, with: {}
   #validates :phone_number, with: {}
@@ -19,14 +19,14 @@ class Contact < ApplicationRecord
 
   validates_format_of :zipcode_1, with: /\A\d{5}\z/, message: "should be five digits long", allow_blank: true
   validates_format_of :zipcode_2, with: /\A\d{5}\z/, message: "should be five digits long", allow_blank: true
-  
+
   #Scopes
   scope :search, ->(term) { where('first_name LIKE ? OR last_name LIKE ?', "#{term}%", "#{term}%") }
   scope :alphabetical, -> { order('first_name', 'last_name') }
-  
+
   #Methods
   def name
     first_name + ' ' + last_name
   end
-  
+
 end
