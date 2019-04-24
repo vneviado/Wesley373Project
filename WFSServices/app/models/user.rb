b6f48b09d054d4009fda309093e1972d5c835425
Lera <vneviado@andrew.cmu.edu>
2019-04-24 19:58:32 -0400

    Fixed user show page with correct info

--- WFSServices/app/models/user.rb
+++ WFSServices/app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   before_save :assign_role
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable, :trackable
 
   def assign_role
   	self.role = "company_staff" if self.role.nil?
