67e69ece23e563a4bef9c8debc3114adc2698baf
Lera <vneviado@andrew.cmu.edu>
2019-04-25 23:58:28 -0400

    made a topbar login and added permissions

--- WFSServices/app/controllers/locations_controller.rb
+++ WFSServices/app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   authorize_resource
 
 
