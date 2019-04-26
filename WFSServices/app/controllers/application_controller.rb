4c9abcb6881659bf85565e4d6d10b5fbc2cc8328
Lera <vneviado@andrew.cmu.edu>
2019-04-26 01:53:12 -0400

    Created_by are now autopopulated

--- WFSServices/app/controllers/application_controller.rb
+++ WFSServices/app/controllers/application_controller.rb
@@ -8,11 +8,6 @@ class ApplicationController < ActionController::Base
 	end
 
 	  private
-	  # Handling authentication
-	  # def current_user
-	  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-	  # end
-	  # helper_method :current_user
 	  
 	  def logged_in?
 	    current_user
