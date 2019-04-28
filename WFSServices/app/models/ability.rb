8fd29031221fa4f4a4b8d56cb35322ad0d2fa216
Sandy Pan <sqp@andrew.cmu.edu>
2019-04-28 15:21:30 -0400

    fixed deleting for service_locaitons and service_contacts

--- WFSServices/app/models/ability.rb
+++ WFSServices/app/models/ability.rb
@@ -60,10 +60,12 @@ class Ability
 
     else 
         # if user is a guest
-        # can read info on camps and curriculums
         can :read, Location
         can :read, Contact
         can :read, Service
+        can :show, Service
+        can :show, Location
+        can :show, Contact
     end
   end
 end
